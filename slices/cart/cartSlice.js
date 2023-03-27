8257ac440b60fc458c316748090bbe81bc752213
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2023-03-27 19:05:26 -0300

    Modified config for production purposes

---
+++ slices/cart/cartSlice.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: {
+    cart: [],
+    status: false,
+    price: 0
+  },
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    add: (state, item) => {
+      let foundItem = state.value.cart.find((i)=> i._id === item.payload._id)
+      if (!foundItem) {
+        state.value.cart.push(item.payload);
+        state.value.price += item.payload.price
+      }
+    },
+    remove: (state, item) => {
+      let filteredCart = state.value.cart.filter(
+        (i) => i._id !== item.payload._id
+      );
+      state.value.cart = filteredCart;
+    },
+    modifyStatus: (state) => {
+      state.value.status = !state.value.status;
+    },
+    emptyCart: (state) => {
+      state.value.cart = [];
+    },
+    // incrementByAmount: (state, action) => {
+    //   state.value += action.payload
+    // },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { add, remove, modifyStatus, emptyCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
